e0cc1d049c9b204605c3ebd2e5786344e7c25334
timoyaj <terimothy@gmail.com>
2026-07-22 17:25:17 +0100

    Update Astro config & deploy workflow to gh-pages branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://timoyaj.github.io',
+  base: '/',
+  output: 'static',
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
